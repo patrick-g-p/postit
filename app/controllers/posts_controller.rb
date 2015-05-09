35991745abf906a43df9a190a9a868c283b55c3c
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-09 13:18:00 -0400

    wired up the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,28 @@
 class PostsController < ApplicationController
+  before_filter :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @posts = Post.find(params[:id])
+
   end
 
   def new
-    @posts = Post.new
+    @post = Post.new
   end
 
   def create
-    @posts = Post.new(post_params)
+    @post = Post.new(post_params)
+
+    if @post.save?
+      flash[:notice] = "Post was submitted."
+      redirect_to @post
+    else
+      @post.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -20,12 +30,21 @@ class PostsController < ApplicationController
   end
 
   def update
-
+    if @post.update(post_params)
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     post.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
