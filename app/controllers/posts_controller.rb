85460c99cf986fbc50e6faf0563c0e5ea3092c8b
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-08 22:31:27 -0400

    added strong parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,11 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @posts = Post.new
   end
 
   def create
-
+    @posts = Post.new(post_params)
   end
 
   def edit
@@ -21,5 +21,11 @@ class PostsController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def post_params
+    post.require(:post).permit(:title, :url, :description)
   end
 end
