dfec3ab61fcc03cb5705f16a270c9e55dc7ea9f9
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-11 18:10:25 -0400

    added helpers to help with presentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def url_fixer(url)
+    if url.starts_with?("http://")
+      url
+    else
+      "http://#{url}"
+    end
+  end
+
+  def date_format_fixer(date)
+    date.strftime(format="%m/%d/%Y at %l:%M%P %Z")
+  end
 end
