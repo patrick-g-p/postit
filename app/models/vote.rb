32cb5aacc1565e3749ba03c4b2786a68f4abc601
Patrick Garrett Pavesi <patrickgpavesi@gmail.com>
2015-05-15 18:35:04 -0400

    minor fix on voting validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
   belongs_to :voteable, polymorphic: true
 
-  validates_uniqueness_of :user_id, scope: :voteable
+  validates_uniqueness_of :user_id, scope: [:voteable_type, :voteable_id]
 end
